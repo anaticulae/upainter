e18f40ddb8c2e376c6e1878c5110ac72f3bbc913
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-02-06 12:01:06 +0100

    test(refactor): clarify test name

--- tests/test_bar.py
+++ tests/test_bar.py
@@ -11,7 +11,7 @@ import painter
 import tests
 
 
-def test_bar_render():
+def test_bar_render_with_labels():
     data = [0, 1, 2, 3, 4, 5, 6, 7]
     rendered = painter.bar_render(
         x=data,
