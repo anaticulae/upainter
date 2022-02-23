2abb304f9e578b395e0dc8ff6a6d42aa50f66733
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-02-23 12:36:57 +0100

    fix(test): adjust image file directory

--- painter/test.py
+++ painter/test.py
@@ -19,7 +19,8 @@ def show_figure(figure, always: bool = False, filename: str = 'painted.png'):
     if not always and not utilatest.single_execution():
         return
     if not utila.exists(figure):
-        with utila.make_tmpdir(root=__file__) as temp:
+        root = utila.path_current(__file__)
+        with utila.make_tmpdir(root=root) as temp:
             png = os.path.join(temp, filename)
             # write png
             if painter.isimage(figure):
