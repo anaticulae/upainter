7f9702c98749e44c29b06388379ba9f1aa6d0a72
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-02-23 12:35:04 +0100

    feat(show): add parameter to change file name

--- painter/test.py
+++ painter/test.py
@@ -15,12 +15,12 @@ import utilatest
 import painter
 
 
-def show_figure(figure, always: bool = False):
+def show_figure(figure, always: bool = False, filename: str = 'painted.png'):
     if not always and not utilatest.single_execution():
         return
     if not utila.exists(figure):
         with utila.make_tmpdir(root=__file__) as temp:
-            png = os.path.join(temp, 'painted.png')
+            png = os.path.join(temp, filename)
             # write png
             if painter.isimage(figure):
                 figure.save(png)
